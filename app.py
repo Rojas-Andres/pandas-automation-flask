d81ccea434feed480de2fe0d4952930ea5717c64
Rojas-Andres <andresfelipe200004@gmail.com>
2021-09-28 18:29:15 -0500

    cambios

--- app.py
+++ app.py
@@ -82,6 +82,5 @@ def upload_file():
     </body>
     </html>
     '''
-
-if __name__ =='__main__':
-    app.run(host="0.0.0.0",port=80,debug=True)
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
